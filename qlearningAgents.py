0e8d51777bf59d4d4b72b2a8cf69402c36dccda8
lesleyzheng <lzheng21@amherst.edu>
2019-10-23 15:05:55 -0400

    changed ordering

--- qlearningAgents.py
+++ qlearningAgents.py
@@ -283,23 +283,25 @@ class ApproximateQAgent(PacmanQAgent):
         s_prime = nextState
         a = action
 
+        # utility of next state
+        a_primes = self.getLegalActions(s_prime)
+        s_prime_q_values = util.Counter()
+        for a_prime in a_primes:
+            s_prime_q_values[a_prime] = self.getQValue(s_prime, a_prime)
+        max_a_prime = s_prime_q_values.argMax()
+        max_val_of_a_prime = s_prime_q_values[max_a_prime]
+        utility_of_s_prime = max_val_of_a_prime
+
         for key in self.weights.keys():
 
-            # utility of next state
-            a_primes = self.getLegalActions(s_prime)
-            max_value = 0
-            for a_prime in a_primes:
-                s_prime_q_value = self.getQValue(s_prime, a_prime)
-                if s_prime_q_value > max_value:
-                    max_value = s_prime_q_value
-            utility_of_s_prime = max_value
-
-            # other
-            q_value = self.getQValue(s, a)
+            # feature_value
             feature_value = self.featExtractor.getFeatures(s, a)[key]
 
+            # difference
+            difference = (r + discount*utility_of_s_prime) - self.getQValue(s, a)
+
             # equation
-            self.weights[key] = self.weights[key] + alpha*(r+discount*utility_of_s_prime-q_value)*feature_value
+            self.weights[key] += alpha*difference*feature_value
 
     def final(self, state):
         "Called at the end of each game."
